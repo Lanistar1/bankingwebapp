18bad043bdabdd7e9e1331308130232235746f27
Emmanuel Otuyemi <emmanuelola92@gmail.com>
2024-06-19 13:13:20 +0100

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Lanistar" };
+  const loggedIn = { firstName: "Lanistar", lastName: "Jackson", email: "contact@test.com" };
 
   return (
     <section className="home">
@@ -22,7 +23,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
     </section>
   );
 };
